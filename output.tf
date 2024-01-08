44d23029c48caf9280da168e63dad182070bac63
Chris Polansky <cpolansky@hashicorp.com>
2024-01-08 23:23:58 +0000

    Adding Outputs

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "organization" {
+  value = tfe_organization.org
+}
+
+output "owners_project" {
+  value = tfe_project.owners_project
+}
+
+output "pmr_project" {
+  value = tfe_project.pmr_project
+}
+
+output "policies_project" {
+  value = tfe_project.policies_project
+}
