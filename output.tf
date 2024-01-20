8b3de226a20fd51875a51ba7702b030e7d3ec1ff
Chris Polansky <cpolansky@hashicorp.com>
2024-01-20 06:47:03 +0000

    Fixes

--- output.tf
+++ output.tf
@@ -2,17 +2,17 @@ output "organization" {
   value = tfe_organization.org
 }
 
-output "owners_project" {
-  value = tfe_project.owners_project
-}
+#output "owners_project" {
+#  value = tfe_project.owners_project
+#}
 
 #output "pmr_project" {
 #  value = tfe_project.pmr_project
 #}
 
-output "policies_project" {
-  value = tfe_project.policies_project
-}
+#output "policies_project" {
+#  value = tfe_project.policies_project
+#}
 
 output "vcs_provider" {
   value = tfe_oauth_client.vcs_client
